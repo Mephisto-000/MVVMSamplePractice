5fb41c051482edf71855f3a72d42ef5388305f73
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-01-05 19:43:24 +0800

    #feat: 新增 FirstViewModel, SecondViewModel

---
+++ WpfMVVMSample/WpfMVVMSample/ViewModels/SecondViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMVVMSample.Models;
+
+
+namespace WpfMVVMSample.ViewModels
+{
+    /// <summary>
+    /// 表示第二個視圖的視圖模型，繼承自 WorkspaceViewModel。
+    /// </summary>
+    public class SecondViewModel : WorkspaceViewModel
+    {
+        /// <summary>
+        /// 視圖的顯示名稱。
+        /// </summary>
+        private const string DisplayViewName = "SecondView";
+
+        /// <summary>
+        /// 只讀的 InfoModel 實例，用於存儲視圖的相關資訊。
+        /// </summary>
+        private readonly InfoModel _info;
+
+        /// <summary>
+        /// 初始化 SecondViewModel 類別的新實例。
+        /// </summary>
+        public SecondViewModel()
+        {
+            // 設定基類的 DisplayName 屬性為視圖的顯示名稱
+            base.DisplayName = DisplayViewName;
+
+            // 如果 _info 尚未初始化，則創建新的 InfoModel 實例
+            if (_info == null)
+            {
+                _info = new InfoModel();
+            }
+            // 設定 _info 的 Name 屬性為視圖的顯示名稱
+            _info.Name = DisplayViewName;
+        }
+
+        /// <summary>
+        /// 獲取 InfoModel 的 Name 屬性值。
+        /// </summary>
+        public string Name
+        {
+            get { return _info.Name; }
+        }
+    }
+}
+
