1918defdb34cc1a1dfd9c593eb830816375f01f2
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-01-05 16:02:38 +0800

    #feat: 新增 Model

---
+++ WpfMVVMSample/WpfMVVMSample/Models/InfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVMSample.Models
+{
+    internal class InfoModel
+    {
+        //畫面名稱
+        public string Name { get; set; }
+    }
+}
