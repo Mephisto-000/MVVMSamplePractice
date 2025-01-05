df9fad363b0b1cc10c1daf605867139ed31d2a33
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-01-05 19:26:38 +0800

    #feat: 新增 WorkspaceViewModel

---
+++ WpfMVVMSample/WpfMVVMSample/ViewModels/WorkspaceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * WorkspaceViewModel 類別繼承自 ViewModelBase。在 MVVM 架構中，WorkspaceViewModel 的主要角色是作為視圖模型的基礎類別，
+ * 提供共用的屬性和方法，供其他具體的視圖模型（如 FirstViewModel、SecondViewModel）繼承和使用。
+*/
+
+namespace WpfMVVMSample.ViewModels
+{
+    public class WorkspaceViewModel : ViewModelBase
+    {
+    }
+}
